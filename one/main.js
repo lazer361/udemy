63c22458fe11f97c7399c3b591105ad5faf6cf37
Sasha <lazer361@mail.ru>
2022-10-28 15:40:01 +0300

    dz_one

---
+++ one/main.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последний просмотренных фильмов?"),
+      b = prompt("На сколько оцените его?"),
+      c = prompt("Один из последний просмотренных фильмов?"),
+      d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
